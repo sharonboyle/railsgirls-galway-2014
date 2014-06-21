dcbc7d50d97abd52bb2b0c5fbb3901d6c238d7d7
Sharon Boyle <sharonconmy@gmail.com>
2014-06-21 12:42:06 +0100

    Initialise railgirls repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailgirlsGalway2014::Application.config.secret_key_base = 'bc97c270fe3011f45d74a00d16939b4f3b700d1bb3da2d877b2618000c341a265110ffb3ada951e6093a3820dd347e0e32faf285955287d5970389facb041ba7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailgirlsGalway2014::Application.config.session_store :cookie_store, key: '_railgirls-galway-2014_session'

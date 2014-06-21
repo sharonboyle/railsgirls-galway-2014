8f62d07c8bfe2d7188a7b80a8f91fd0971b64529
Sharon Boyle <sharonconmy@gmail.com>
2014-06-21 16:21:06 +0100

    Initialise railgirls update id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140621141625) do
+ActiveRecord::Schema.define(version: 20140621150352) do
+
+  create_table "places", force: true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.decimal  "latitude"
+    t.decimal  "longitude"
+    t.text     "description"
+    t.string   "picture"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false

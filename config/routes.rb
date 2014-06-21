cc286231a7c4a53a08c108ab7aef876077a28da1
Sharon Boyle <sharonconmy@gmail.com>
2014-06-21 15:43:30 +0100

    adding authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RailgirlsGalway2014::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
